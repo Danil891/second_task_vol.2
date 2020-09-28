a5a0baaa2280c40a7d4d7d3a9ba132e1de162257
Danil <cherepkov.dd@edu.spbstu.ru>
2020-09-28 10:44:30 +0300

    production It remains to add one method and optimize + tests

--- src/InUniq.java
+++ src/InUniq.java
@@ -1,1 +1,44 @@
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
+import java.io.IOException;
+
+public class InUniq {
+    @Option(name = "-i")
+    private boolean ignoreRegister = false;
+
+    @Option(name = "-u")
+    private boolean printOnlyUniqLines = false;
+
+    @Option(name = "-c")
+    private boolean printCountOfLines = false;
+
+    @Option(name = "-s")
+    private int numIgnoreChars = 1;
+
+    @Option(name = "-o")
+    private String outFile = "";
+
+    @Argument()
+    private String inFile = "";
+
+    public static void main(String[] args) throws IOException {
+        new InUniq().launch(args);
+    }
+
+    public void launch(String[] args) throws IOException {
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err.println("You are mistaken");
+            parser.printUsage(System.err);
+            System.exit(-2);
+        }
+        Uniq uniq = new Uniq(ignoreRegister, printOnlyUniqLines, printCountOfLines, numIgnoreChars,  outFile, inFile);
+        uniq.runUniq();
+    }
+}
--- src/Uniq.java
+++ src/Uniq.java
@@ -1,4 +1,108 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Uniq {
+    private boolean ignoreRegister;
+    private boolean printOnlyUniqLines;
+    private boolean printCountOfLines;
+    private int numIgnoreChars;
+    private String outFile;
+    private String inFile;
+    private String newString;
 
 
+    public Uniq(boolean ignoreRegister, boolean printOnlyUniqLines, boolean printCountOfLines,
+                int numIgnoreChars, String outFile, String inFile) {
+        this.ignoreRegister = ignoreRegister;
+        this.printOnlyUniqLines = printOnlyUniqLines;
+        this.printCountOfLines = printCountOfLines;
+        this.numIgnoreChars = numIgnoreChars;
+        this.outFile = outFile;
+        this.inFile = inFile;
+    }
+
+    public void runUniq() throws IOException {
+        int counterUniq = 0;
+        int counter = 1;
+        ArrayList<Integer> listOfCounters = new ArrayList<>();
+        ArrayList<String> strings = new ArrayList<>();
+
+        Scanner in = new Scanner(System.in);
+        if (!inFile.equals("")) {
+            strings.addAll(Files.readAllLines(Paths.get(inFile)));
+        } else {
+            String line = in.nextLine();
+            strings.add(line);
+        }
+
+        ArrayList<String> newStrings = new ArrayList<>();
+        newString = strings.get(0);
+        newStrings.add(newString);
+        if (!printCountOfLines) {
+
+            if (!ignoreRegister) {
+
+                for (int i = 1; i < strings.size(); i++)
+                    if (!newString.substring(numIgnoreChars - 1).
+                            equals(strings.get(i).substring(numIgnoreChars - 1))) {
+                        newStrings.add(strings.get(i));
+                        newString = strings.get(i);
+                    }
+
+            } else {
+                for (int i = 1; i < strings.size(); i++)
+                    if (!newString.substring(numIgnoreChars - 1).toLowerCase()
+                            .equals(strings.get(i).substring(numIgnoreChars - 1).toLowerCase())) {
+                        newStrings.add(strings.get(i));
+                        newString = strings.get(i);
+                    }
+            }
+
+        } else {
+            if (!ignoreRegister) {
+
+                for (int i = 1; i < strings.size(); i++)
+                    if (!newString.substring(numIgnoreChars - 1).
+                            equals(strings.get(i).substring(numIgnoreChars - 1))) {
+                        newStrings.set(newStrings.size() - 1 , counter + " " + newString);
+                        newString = strings.get(i);
+                        counter = 1;
+                        newStrings.add(newString);
+                    } else counter++ ;
+
+            } else {
+                for (int i = 1; i < strings.size(); i++)
+                    if (!newString.substring(numIgnoreChars - 1).toLowerCase()
+                            .equals(strings.get(i).substring(numIgnoreChars - 1).toLowerCase())) {
+                        newStrings.set(newStrings.size() - 1 , counter + " " + newString);
+                        newString = strings.get(i);
+                        counter = 1;
+                        newStrings.add(newString);
+                    } else counter++ ;
+            }
+            newStrings.set(newStrings.size() - 1 , counter + " " + newString);
+        }
+
+        if (!outFile.equals("")) {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File(outFile)));
+            for (String element : strings) {
+                writer.write(element);
+                writer.write("\n");
+            }
+        } else {
+            for (String element : newStrings) {
+                System.out.println(element);
+            }
+        }
+
+
+    }
 }
+
+
